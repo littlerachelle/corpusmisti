2a4fb3973dd15be6a98fec9f55241e458dccee66
Unknown <rachel@klang-games.com>
2017-12-09 15:34:25 +0100

    Fixing movement for the rift

---
+++ GearVR2017/Assets/GameJam/RiftControllerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiftControllerMovement : MonoBehaviour {
+
+    public Transform headTransform;
+
+    public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //UnityEngine.VR
+        Vector2 primaryAxis = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        if (primaryAxis.magnitude > 0.1f)
+        {
+            Vector3 move = new Vector3(primaryAxis.x, 0.0f, primaryAxis.y) * speed;
+            Vector3 relMove = headTransform.TransformVector(move);
+            GetComponent<CharacterController>().Move(relMove);
+        }
+    }
+}
