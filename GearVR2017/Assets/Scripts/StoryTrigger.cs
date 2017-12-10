b12af59103ccfe30037cde9df7505b7b7d195b75
Unknown <rachel@klang-games.com>
2017-12-10 10:16:13 +0100

    Adding story trigger script

---
+++ GearVR2017/Assets/Scripts/StoryTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryTrigger : MonoBehaviour {
+
+    public AudioClip storyClip;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<CharacterController>() != null)
+        {
+            if (storyClip != null)
+            {
+                AudioSource source = GetComponent<AudioSource>();
+                source.PlayOneShot(storyClip);
+            }
+        }
+    }
+}
